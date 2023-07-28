b41dee1a161fce84e2ee6c9d47ac19916a02d9e4
Vladimir Fibe <motiw@icloud.com>
2023-07-28 22:54:01 +0600

    Initial Commit

---
+++ MessengerTutorial/MessengerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTutorialApp.swift
+//  MessengerTutorial
+//
+//  Created by Vladimir Fibe on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
